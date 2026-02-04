36625037f38d70abe6b739b556a8ac98e004ca65
Felix Kästner <felix.kaestner@sap.com>
2026-02-04 17:10:08 +0100

    Add missing license headers

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2026 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
