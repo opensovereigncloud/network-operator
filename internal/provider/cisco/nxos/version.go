be81d147ab4c08fdde4251ff3de5ef8a360bfd8f
Felix Kästner <felix.kaestner@sap.com>
2026-06-18 16:49:00 +0200

    Use ordered iota for NX-OS version comparison
    The Version type was a plain string, which meant comparisons like
    VersionUnknown < VersionNX10_7_1 were lexicographic and incorrect
    ("Unknown" > "10.7(1)"). Switch to uint8 with iota so that
    relational operators reflect actual release ordering.
    
    Signed-off-by: Felix Kästner <felix.kaestner@sap.com>

--- internal/provider/cisco/nxos/version.go
+++ internal/provider/cisco/nxos/version.go
@@ -6,21 +6,54 @@ package nxos
 import "github.com/ironcore-dev/network-operator/internal/transport/gnmiext"
 
 // Version represents the operating system version of the target device.
-type Version string
+// Versions are ordered so that comparison operators (>, >=, <, <=) reflect
+// the actual release ordering.
+type Version uint8
 
 const (
-	VersionUnknown  Version = "Unknown"
-	VersionNX10_4_3 Version = "10.4(3)"
-	VersionNX10_4_4 Version = "10.4(4)"
-	VersionNX10_4_5 Version = "10.4(5)"
-	VersionNX10_4_6 Version = "10.4(6)"
-	VersionNX10_5_1 Version = "10.5(1)"
-	VersionNX10_5_2 Version = "10.5(2)"
-	VersionNX10_5_3 Version = "10.5(3)"
-	VersionNX10_6_1 Version = "10.6(1)"
-	VersionNX10_6_2 Version = "10.6(2)"
-	VersionNX10_6_3 Version = "10.6(3)"
+	VersionUnknown  Version = iota
+	VersionNX10_4_3         // 10.4(3)
+	VersionNX10_4_4         // 10.4(4)
+	VersionNX10_4_5         // 10.4(5)
+	VersionNX10_4_6         // 10.4(6)
+	VersionNX10_5_1         // 10.5(1)
+	VersionNX10_5_2         // 10.5(2)
+	VersionNX10_5_3         // 10.5(3)
+	VersionNX10_6_1         // 10.6(1)
+	VersionNX10_6_2         // 10.6(2)
+	VersionNX10_6_3         // 10.6(3)
+
+	VersionNX10_7_1 // 10.7(1)
 )
+
+func (v Version) String() string {
+	switch v {
+	case VersionNX10_4_3:
+		return "10.4(3)"
+	case VersionNX10_4_4:
+		return "10.4(4)"
+	case VersionNX10_4_5:
+		return "10.4(5)"
+	case VersionNX10_4_6:
+		return "10.4(6)"
+	case VersionNX10_5_1:
+		return "10.5(1)"
+	case VersionNX10_5_2:
+		return "10.5(2)"
+	case VersionNX10_5_3:
+		return "10.5(3)"
+	case VersionNX10_6_1:
+		return "10.6(1)"
+	case VersionNX10_6_2:
+		return "10.6(2)"
+	case VersionNX10_6_3:
+		return "10.6(3)"
+	case VersionNX10_7_1:
+		return "10.7(1)"
+	default:
+		return "Unknown"
+	}
+}
 
 // nxosVersions maps the revision date of the Cisco-NX-OS-device yang model to the corresponding [Version].
 // It is used to determine the version of the target device based on the capabilities returned by the device.
@@ -35,6 +68,10 @@ var nxosVersions = map[string]Version{
 	"2025-08-12": VersionNX10_6_1,
 	"2025-12-12": VersionNX10_6_2,
 	"2026-04-24": VersionNX10_6_3,
+
+	// VersionNX10_7_1 is the minimum version that supports CA chains via gNOI LoadCertificate.
+	// TODO: Update with the correct YANG model revision date once NX-OS 10.7(1) is released.
+	"9999-01-01": VersionNX10_7_1,
 }
 
 // NXVersion returns the NX-OS operating system version of the target device based on the supported models.
